17712272b5071b00734756f99f9f4ad92bb3a6ec
Craig Thacker <craigthackerx@gmail.com>
2021-09-19 16:19:32 +0100

    update

--- docs/assets/js/chooseimage.js
+++ docs/assets/js/chooseimage.js
@@ -1,15 +1,15 @@
-window.onload = chooseImage;
-
 let images = [
-    "assets/memes/meme1.jpg",
-    "assets/memes/meme2.jpg",
-    "assets/memes/meme3.jpg",
-    "assets/memes/meme4.jpg",
-    "assets/memes/meme5.jpg",
-    "assets/memes/meme6.jpg"
+    "/assets/memes/meme1.jpg",
+    "/assets/memes/meme2.jpg",
+    "/assets/memes/meme3.jpg",
+    "/assets/memes/meme4.jpg",
+    "/assets/memes/meme5.jpg",
+    "/assets/memes/meme6.jpg"
 ];
 
 function chooseImage() {
     let randomNumber = Math.floor(Math.random() * images.length);
     document.getElementById("image-fetch").src = images[randomNumber];
 }
+
+window.onload = chooseImage;
