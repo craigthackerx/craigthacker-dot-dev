7768a704657cad5718f75ce463db629357bab878
Craig Thacker <craigthackerx@gmail.com>
2021-09-19 16:33:02 +0100

    update

--- docs/assets/js/chooseimage.js
+++ docs/assets/js/chooseimage.js
@@ -1,15 +1,13 @@
-let images = [
+let memes = [
     "/assets/memes/meme1.jpg",
     "/assets/memes/meme2.jpg",
     "/assets/memes/meme3.jpg",
     "/assets/memes/meme4.jpg",
     "/assets/memes/meme5.jpg",
-    "/assets/memes/meme6.jpg"
+    ".assets/memes/meme6.jpg"
 ];
 
-function chooseImage() {
-    let randomNumber = Math.floor(Math.random() * images.length);
-    document.getElementById("image-fetch").src = images[randomNumber];
+window.onload = function chooseMeme() {
+    let randomNumber = Math.floor(Math.random() * memes.length);
+    document.getElementById("image-fetch").src = memes[randomNumber];
 }
-
-window.onload = chooseImage;
