2b8d10dbd9f080c4af5d12d2a56e6c30c30f409b
Craig Thacker <craigthackerx@gmail.com>
2021-09-19 16:34:59 +0100

    update

--- docs/assets/js/chooseimage.js
+++ docs/assets/js/chooseimage.js
@@ -1,13 +1,12 @@
-let memes = [
-    "/assets/memes/meme1.jpg",
-    "/assets/memes/meme2.jpg",
-    "/assets/memes/meme3.jpg",
-    "/assets/memes/meme4.jpg",
-    "/assets/memes/meme5.jpg",
-    ".assets/memes/meme6.jpg"
-];
-
 window.onload = function chooseMeme() {
+    let memes = [
+        "/assets/memes/meme1.jpg",
+        "/assets/memes/meme2.jpg",
+        "/assets/memes/meme3.jpg",
+        "/assets/memes/meme4.jpg",
+        "/assets/memes/meme5.jpg",
+        ".assets/memes/meme6.jpg"
+    ];
     let randomNumber = Math.floor(Math.random() * memes.length);
     document.getElementById("image-fetch").src = memes[randomNumber];
 }
