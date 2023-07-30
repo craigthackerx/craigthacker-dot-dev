f35f7b6194f8b4d8b0f148b221153f94a0531693
Craig Thacker <craigthackerx@gmail.com>
2023-07-30 15:48:57 +0100

    update json and js

--- docs/assets/js/chooseimage.js
+++ docs/assets/js/chooseimage.js
@@ -1,7 +1,7 @@
 fetch('/images.json')
     .then(response => response.json())
     .then(data => {
-        const memes = data.images;
+        const media = data.media;
 
         // Get all img elements in your document
         const images = document.getElementsByTagName("img");
@@ -10,8 +10,32 @@ fetch('/images.json')
             // Change src attribute only if it ends in .jpg, .png, or .gif
             let ext = img.src.split('.').pop();
             if (ext === "jpg" || ext === "png" || ext === "gif") {
-                let randomNumber = Math.floor(Math.random() * memes.length);
-                img.src = memes[randomNumber];
+                let randomIndex;
+                let lastMedia = sessionStorage.getItem(`media-${i}`);
+                do {
+                    randomIndex = Math.floor(Math.random() * media.length);
+                } while (media[randomIndex] === lastMedia);
+                sessionStorage.setItem(`media-${i}`, media[randomIndex]);
+                img.src = media[randomIndex];
+            }
+        }
+
+        // Get all video elements in your document
+        const videos = document.getElementsByTagName("video");
+        for(let i=0; i<videos.length; i++) {
+            let video = videos[i];
+            let source = video.getElementsByTagName("source")[0];
+            // Change src attribute only if it ends in .mp4
+            let ext = source.src.split('.').pop();
+            if (ext === "mp4") {
+                let randomIndex;
+                let lastMedia = sessionStorage.getItem(`media-${images.length + i}`);
+                do {
+                    randomIndex = Math.floor(Math.random() * media.length);
+                } while (media[randomIndex] === lastMedia || media[randomIndex].split('.').pop() !== 'mp4');
+                sessionStorage.setItem(`media-${images.length + i}`, media[randomIndex]);
+                source.src = media[randomIndex];
+                video.load();
             }
         }
     });
