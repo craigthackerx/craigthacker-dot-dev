28985fef682a1cc3997006b74465e64afe2a866e
Craig Thacker <craigthackerx@gmail.com>
2023-07-30 15:42:53 +0100

    update js

--- docs/assets/js/chooseimage.js
+++ docs/assets/js/chooseimage.js
@@ -1,19 +1,17 @@
-window.onload = function chooseMeme() {
-    const memes = [
-        "/assets/memes/meme1.jpg",
-        "/assets/memes/meme2.jpg",
-        "/assets/memes/meme3.jpg",
-        "/assets/memes/meme4.jpg",
-        "/assets/memes/meme5.jpg",
-        "/assets/memes/meme6.jpg",
-        "/assets/memes/meme7.jpg",
-        "/assets/memes/meme8.jpg",
-        "/assets/memes/meme9.jpg",
-        "/assets/memes/meme10.jpg",
-        "/assets/memes/meme11.jpg",
-        "/assets/memes/meme12.jpg",
-        "/assets/memes/meme13.jpg",
-    ];
-    let randomNumber = Math.floor(Math.random() * memes.length);
-    document.getElementById("image-fetch").src = memes[randomNumber];
-}
+fetch('/images.json')
+    .then(response => response.json())
+    .then(data => {
+        const memes = data.images;
+
+        // Get all img elements in your document
+        const images = document.getElementsByTagName("img");
+        for(let i=0; i<images.length; i++) {
+            let img = images[i];
+            // Change src attribute only if it ends in .jpg, .png, or .gif
+            let ext = img.src.split('.').pop();
+            if (ext === "jpg" || ext === "png" || ext === "gif") {
+                let randomNumber = Math.floor(Math.random() * memes.length);
+                img.src = memes[randomNumber];
+            }
+        }
+    });
